936da8fbad047f076ad937eed5686687bf73d48a
Oliver-DA <71343538+Oliver-DA@users.noreply.github.com>
2021-01-03 17:27:11 -0500

    header links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,7 +9,10 @@ const Header = () => {
     return (
       <div className="header">
         <div className="bounds">
-          <h1 className="header--logo">Courses</h1>
+          <Link to = "/">
+            <h1 className="header--logo">Courses</h1>
+          </Link>
+
           <nav>
 
             {authUser ? (
